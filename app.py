87c85021de76a989d565829ae6c822ad7d00e75e
xshj0403 <214313@clvsc.tyc.edu.tw>
2025-02-13 14:03:51 +0800

    V2

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'hello world'
+# 從環境變數取得 LINE Bot Token 和 Secret
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+line_handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "LINE Bot is running on Vercel."
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+
+    try:
+        line_handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return "OK"
+
+@line_handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_message = event.message.text
+    reply_message = f"你說了: {user_message}"
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
+
+# 讓 Vercel 正確啟動 Flask
+def line_handler(event, context):
+    return app(event, context)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
